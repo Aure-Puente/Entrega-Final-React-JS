89c84b67d71b98db29fadc283c3b12af813b8ef6
Aurelio Puente <aurelianopuente@hotmail.com>
2024-03-26 14:25:46 -0300

    estructura armada

---
+++ src/components/layout/navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+export const Navbar = () => {
+    return (
+        <div>
+            <h3>Logo</h3>
+            <h2>GUITAR SHOPPING</h2>
+            <ul>
+                <li><a href="#">Eléctricas</a></li>
+                <li><a href="#">Acústicas</a></li>
+                <li><a href="#">Electroacústicas</a></li>
+            </ul>
+        </div>
+    )
+}
